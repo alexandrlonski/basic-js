7f28c0897a0248d2ca3a57012b9b8f31bb5cb9e2
Alexandr <aleksandr.lonski.86@gmail.com>
2021-03-13 11:27:27 +0300

    feat: completed the tramsform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =function transform( arr ) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != "--double-next" && arr[i] != "--double-prev") ?
+      newArr.push(arr[i]):
+        (arr[i] == '--discard-next' && i != arr.length - 1) ?
+          i++ :
+            (arr[i] == '--discard-prev' && arr[i - 2] != '--discard-next' && i != 0) ?
+              newArr.pop():
+                (arr[i] == '--double-next' && i != arr.length - 1) ?
+                  newArr.push(arr[i + 1]):
+                    (arr[i] == '--double-prev' && arr[i - 2] != '--discard-next' && i != 0) ?
+                      newArr.push(arr[i - 1]) : null;
+    
+  }
+
+  return newArr;
+   
 };
+
+
