4db6b814c343ebe610e24dff34d21ebae7072e97
Alexandr <aleksandr.lonski.86@gmail.com>
2021-03-20 18:59:06 +0300

    completed task vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  }
+
+  encrypt(str, key) {
+    const arrStr = [...str.toUpperCase()];
+    const allKey = [...key.toUpperCase()];
+    let i = 0;
+    let result = arrStr.map( el => {
+      if (this.alphabet.includes(el)) {
+        let index = (this.alphabet.indexOf(allKey[i]) + this.alphabet.indexOf(el)) % this.alphabet.length;
+        i = ++i % allKey.length;
+        return this.alphabet[index];
+      }
+      return el;
+    } );
+    return this.direct ? result.join('') : result.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    const arrStr = [...str.toUpperCase()];
+    const allKey = [...key.toUpperCase()];
+    let i = 0;
+    let result = arrStr.map( el => {
+      if (this.alphabet.includes(el)) {
+        let index = this.alphabet.indexOf(el) - this.alphabet.indexOf(allKey[i]);
+        if (index < 0) index += this.alphabet.length;
+        i = ++i % allKey.length;
+        return this.alphabet[index];
+      }
+      return el;
+    } );
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+
