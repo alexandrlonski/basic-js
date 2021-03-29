8376e6b9cc4254be448e3c7f21f68a944db2b50f
Alexandr <aleksandr.lonski.86@gmail.com>
2021-03-29 22:43:35 +0300

    fix: simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,34 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-chain : [],
-  getLength() {
-    return this.chain.length;
-  },
-  addLink(value) {
-    if(value == undefined && value != null) value = '';
-    this.chain.push(`( ${value} )`);
-    return this;
-  },
-  removeLink(position) {
-    if(position < 0 || position > this.chain.length || !Number.isInteger(position)){
-       throw new Error();
-    }
-    this.chain.splice(position - 1, 1);
-    return this;
-  },
-  reverseChain() {
-    this.chain.reverse()
-    return this;
-  },
-  finishChain() {
-    str = `${this.chain.join('~~')}`;
-    this.chain.splice(0);
-    return str
-    
-  }
+	chain: [],
+	getLength() {
+		return this.chain.length;
+	},
+	addLink(value) {
+		if (value == undefined && value != null) value = "";
+		this.chain.push(`( ${value} )`);
+		return this;
+	},
+	removeLink(position) {
+		if (
+			position < 0 ||
+			position > this.chain.length ||
+			!Number.isInteger(position)
+		) {
+			this.chain = [];
+			throw new Error();
+		}
+		this.chain.splice(position - 1, 1);
+		return this;
+	},
+	reverseChain() {
+		this.chain.reverse();
+		return this;
+	},
+	finishChain() {
+		let str = `${this.chain.join("~~")}`;
+		this.chain.splice(0);
+		return str;
+	},
 };
 
 module.exports = chainMaker;
-
-
